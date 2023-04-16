2555f27e709c4bfbb110b20e733d3798cacabcdc
FakeApate <imboden.samuel@protonmail.ch>
2023-04-16 09:42:31 +0200

    change file icon extractor

--- backend.js
+++ backend.js
@@ -3,7 +3,7 @@ const server = http.createServer();
 const { Server } = require("socket.io");
 const io = new Server(server);
 const fs = require("fs");
-const fileIcon = require("extract-file-icon");
+const icon = require('file-icon-extractor');
 const exiftool = require("exiftool-vendored").exiftool;
 
 class MixerInfo {
@@ -14,7 +14,7 @@ class MixerInfo {
     });
   }
 
-  read() {
+  async read() {
     delete this.list;
     this.userSettPath = `${process.env.APPDATA}/midi-mixer-app/user-settings.json`;
     this.userSettings = JSON.parse(fs.readFileSync(this.userSettPath, "utf-8"));
@@ -37,16 +37,16 @@ class MixerInfo {
       const groupNum = this.profile.groups[key].name.split(" ")[1];
       if (group.type === "session") {
         const stats = await exiftool.read(group.paths[0]);
-        const buffer = await fileIcon(this.device.groups[key][0].paths[0]);
-        const image = `data:image/jpg;base64,${buffer.toString("base64")}`;
+        icon.extract(this.device.groups[key][0].paths[0],"");
+        const img = `data:image/jpg;base64,${fs.readFileSync(stats.FileName.replace("exe","png")).toString("base64")}`
         if (this.list) {
           this.list.push({
             num: groupNum,
             name: stats.ProductName,
-            icon: image,
+            icon: img,
           });
         } else {
-          this.list = [{ num: groupNum, name: stats.ProductName, icon: image }];
+          this.list = [{ num: groupNum, name: stats.ProductName, icon: img }];
         }
       }
     }
